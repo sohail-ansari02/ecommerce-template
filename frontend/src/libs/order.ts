3d3d3029197e23b1417f51c804203283e5ad8e90
sohail-ansari02 <syansari02@gmail.com>
2024-11-06 10:36:19 +0530

    .

--- frontend/src/libs/order.ts
+++ frontend/src/libs/order.ts
@@ -1,7 +1,7 @@
 import { toast } from "sonner";
 
 export async function OrderProduct(data: any = undefined) {
-  const response = await fetch("https://api.web3forms.com/submit", {
+  await fetch("https://api.web3forms.com/submit", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -11,14 +11,13 @@ export async function OrderProduct(data: any = undefined) {
       access_key: "587eb20c-6a0d-47f4-ae13-88cdccab0881",
       name: "New Order",
       email: "miraclefittness@gmail.com",
-      message: "test",
-      "order info": JSON.stringify(data),
+      message: "test" + JSON.stringify(data),
     }),
   }).then((response) => {
     const result = response.json();
     // @ts-ignore
     if (result.success) {
-      console.log(result);
+      console.log('result',response);
       toast.success("Successfully Order Placed, we will contact you shortly");
     }
 });
