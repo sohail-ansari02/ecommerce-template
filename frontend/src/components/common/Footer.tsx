766c28625dcfe2b8a9383e6563158d7024e20ac8
sohail-ansari02 <syansari02@gmail.com>
2024-11-14 23:47:29 +0530

    .

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -33,7 +33,7 @@ export const Footer = () => {
               © {new Date().getFullYear()}
             </span>
           </div>
-          <div className="flex gap-3.5">
+          <div className="flex gap-3.5 justify-start">
             {/* <Link
               href="https://www.linkedin.com/in/marcospenelascamara/"
               target="_blank"
